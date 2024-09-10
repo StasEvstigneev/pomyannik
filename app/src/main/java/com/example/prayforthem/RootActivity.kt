171d51be212756ade9f11265825ad8a2893f3827
Stas <137781307+StasEvstigneev@users.noreply.github.com>
2024-09-10 21:37:39 +0300

    FragmentContainerView, Toolbar style

--- app/src/main/java/com/example/prayforthem/RootActivity.kt
+++ app/src/main/java/com/example/prayforthem/RootActivity.kt
@@ -3,9 +3,9 @@ package com.example.prayforthem
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_root)
     }
 }
