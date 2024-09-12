ecb8b2b9175455ecb3ba105ab3f86d596bea1f9b
StasEvstigneev <evstigneevstas@gmail.com>
2024-09-12 21:07:29 +0300

    Koin DI implementation

--- app/src/main/java/com/example/prayforthem/App.kt
+++ app/src/main/java/com/example/prayforthem/App.kt
@@ -1,9 +1,19 @@
 package com.example.prayforthem
 
 import android.app.Application
+import com.example.prayforthem.di.dataModule
+import com.example.prayforthem.di.interactorModule
+import com.example.prayforthem.di.repositoryModule
+import com.example.prayforthem.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(interactorModule, dataModule, repositoryModule, viewModelModule)
+        }
     }
 }
