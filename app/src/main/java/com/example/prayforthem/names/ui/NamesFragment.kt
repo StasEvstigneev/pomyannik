df01921e969b6bc8ffa9c956b3c3039fe6a72658
Stas <137781307+StasEvstigneev@users.noreply.github.com>
2024-09-27 14:22:24 +0300

    NameFragment, Autocomplete TextView ver.1

--- app/src/main/java/com/example/prayforthem/names/ui/NamesFragment.kt
+++ app/src/main/java/com/example/prayforthem/names/ui/NamesFragment.kt
@@ -1,9 +1,14 @@
 package com.example.prayforthem.names.ui
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.example.prayforthem.R
 import com.example.prayforthem.RootActivity
@@ -18,6 +23,68 @@ class NamesFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel by viewModel<NamesViewModel>()
 
+    private var dignity = ""
+    private var name = ""
+
+    private var dignityList = listOf(
+        "Патриарх",
+        "Митрополит",
+        "Епископ",
+        "Архимандрит",
+        "Иерей",
+        "Диакон",
+        "Монах",
+        "Патриарх",
+        "Митрополит",
+        "Епископ",
+        "Архимандрит",
+        "Иерей",
+        "Диакон",
+        "Монах",
+        "Патриарх",
+        "Митрополит",
+        "Епископ",
+        "Архимандрит",
+        "Иерей",
+        "Диакон",
+        "Монах",
+        "Патриарх",
+        "Митрополит",
+        "Епископ",
+        "Архимандрит",
+        "Иерей",
+        "Диакон",
+        "Монах"
+    )
+
+    private var namesList = listOf(
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя",
+        "Имя"
+    )
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,6 +92,37 @@ class NamesFragment : Fragment() {
         setFragmentTitle(requireActivity() as RootActivity, getString(R.string.names))
         _binding = FragmentNamesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dignityAdapter =
+            ArrayAdapter<String>(requireContext(), R.layout.name_drop_down_item, dignityList)
+
+        val namesAdapter =
+            ArrayAdapter<String>(requireContext(), R.layout.name_drop_down_item, namesList)
+
+        binding.inputDignity.apply {
+            setAdapter(dignityAdapter)
+            setDropDownBackgroundDrawable(ColorDrawable(Color.WHITE)) // убирает верхние марджины в dpopdown menu
+            doAfterTextChanged { text ->
+                dignity = if (text.isNullOrBlank()) "" else text.toString()
+            }
+        }
+
+        binding.inputName.apply {
+            setAdapter(namesAdapter)
+            setDropDownBackgroundDrawable(ColorDrawable(Color.WHITE))
+            doAfterTextChanged { text ->
+                name = if (text.isNullOrBlank()) "" else text.toString()
+            }
+        }
+
+        binding.buttonSave.setOnClickListener {
+            Toast.makeText(requireContext(), dignity + " " + name, Toast.LENGTH_LONG).show()
+        }
+
     }
 
     override fun onDestroyView() {
