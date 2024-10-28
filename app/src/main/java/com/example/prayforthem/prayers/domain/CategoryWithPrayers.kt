fd5598cff6f265e3ee0c62244126c44f7c6a69d5
Stas <137781307+StasEvstigneev@users.noreply.github.com>
2024-10-28 16:41:22 +0300

    Prayer Items layouts

--- app/src/main/java/com/example/prayforthem/prayers/domain/CategoryWithPrayers.kt
+++ app/src/main/java/com/example/prayforthem/prayers/domain/CategoryWithPrayers.kt
@@ -1,6 +1,5 @@
 package com.example.prayforthem.prayers.domain
 
-import com.example.prayforthem.prayerdisplay.domain.Prayer
 import com.example.prayforthem.prayerscategories.domain.PrayerCategory
 
 data class CategoryWithPrayers(
